14605d8cfe47e5499b0f70adfdf1533896abaec1
Frederick Emerson <frederick82004@gmail.com>
2024-04-23 01:58:13 +0800

    added styling to the homepage

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -4,7 +4,7 @@ export function TopNav() {
     return(
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>
-        Fred Really Loves To Yap
+        Fred Loves To Rant
       </div>
   
         <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { db } from "~/server/db";
 export const dynamic = "force-dynamic";
+import { CardTitle, CardDescription, CardHeader, CardContent, Card } from "../components/ui/card"
 
 export interface BlogPost {
   id: number;
@@ -17,19 +18,24 @@ export default async function HomePage() {
     orderBy: (model, {desc}) => desc(model.id)
   });
 
-  console.log(posts)
-
   return (
-    <main className="">
+    <main className="px-8 py-4">
+      <div className="mb-12 ">
+        <h1 className="text-6xl font-extrabold tracking-tight"> Fred Really Loves to Yap, Doesn't He?</h1>
+        <p className="mt-4 text-lg text-gray-500"> Check Out, his latest rants below</p>
+      </div>
+      
       <div className="flex flex-wrap gap-4">
         {
         posts.map((post,index) => (
           
             <Link key={post.slug}  href={"blogs/"+post.slug} >    
-              <div className="w-48"> 
-              <img src={post.url} />
-              <h1>  {post.title} </h1>
-              </div>
+               <Card className="bg-gray-900 pt-4   border-0 rounded-xl shadow-2xl dark:bg-gray-1000">
+                  <CardContent className=" flex flex-col justify-center items-center rounded-t-xl">
+                   <img className="w-48" src={post.url} />
+                  <CardTitle className="mt-2 text-white text-md">{post.title}</CardTitle>
+                  </CardContent>
+                </Card>
             </Link>
 
          
