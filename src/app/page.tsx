843f7887997ce9528f8303c34ba202a4448e94cb
Frederick Emerson <frederick82004@gmail.com>
2024-04-26 14:17:39 +0800

    better loading ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,7 +30,15 @@ export default async function HomePage() {
       <div className="flex flex-wrap gap-4  ">
         {
         posts.map((post,index) => (
-          
+          <Suspense fallback={()=>(
+            <div className="bg-gray-900 pt-4 border-0 rounded-xl shadow-2xl dark:bg-gray-1000">
+              <div className="animate-pulse flex flex-col justify-center items-center rounded-t-xl">
+                <div className="w-48 h-48 rounded-lg bg-gray-600"></div> 
+                <div className="mt-2 h-6 w-48 bg-gray-600"></div> 
+              </div>
+            </div>
+          )}>
+
             <Link key={post.slug}  href={"blogs/"+post.slug} >    
                <Card className="bg-gray-900 pt-4   border-0 rounded-xl shadow-2xl dark:bg-gray-1000">
                   <CardContent className=" flex flex-col justify-center items-center rounded-t-xl">
@@ -39,6 +47,7 @@ export default async function HomePage() {
                   </CardContent>
                 </Card>
             </Link>
+          </Suspense>
         ))
       }
       </div>
