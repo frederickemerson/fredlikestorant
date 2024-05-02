24309ef55c9a69c444938ee90cc14de5d30bf7f5
Frederick Emerson <frederick82004@gmail.com>
2024-05-02 10:38:08 +0800

    more design changes

--- src/app/blogs/[...slug]/page.tsx
+++ src/app/blogs/[...slug]/page.tsx
@@ -16,29 +16,25 @@ export default async function Post({params} : props) {
     })
 
   return (
-      <div className="flex flex-col w-full h-screen px-12 py-6 ">
-        <div className="flex flex-col">
+      <div className="flex flex-col items-center w-full h-screen px-12 py-6 ">
+        <div className="flex flex-col w-9/10 lg:w-1/2">
           <div className="flex flex-col mb-12">
-            <h1 className="text-4xl font-extrabold tracking-tight lg:text-6xl xl:text-7xl">{blogpost?.title}</h1>
-            <p className="text-sm text-gray-500 dark:text-gray-400">Posted on {new Date(blogpost?.createdAt).toLocaleDateString()}</p>
+            <h1 className="text-5xl font-extrabold tracking-tight md:text-6xl xl:text-7xl">{blogpost?.title}</h1>
+            <p className="align-right text-sm text-gray-500 dark:text-gray-400">Posted on {new Date(blogpost?.createdAt).toLocaleDateString()}</p>
           </div>
-          <div className=" border-indigo-300">
+          <div className=" flex flex-col items-center border-indigo-300">
               <img
                 alt="Cover image"
                 className="aspect-video overflow-hidden rounded-lg object-cover mb-4 lg:mr-2"
                 height={140}
                 src={blogpost?.url}
                 width={650}
-                align="left"
               />
             <MDXRemote source={blogpost?.content} />
           </div>
         </div>
-        <div className="flex flex-col mt-4">
-          <div className='flex justify-between'>
-          <h1 className="text-3xl font-extrabold tracking-tight lg:text-4xl xl:text-5xl">Comments</h1>
+        <div className="mt-4 float-right md:fixed bottom-4 md:right-1 md:-translate-x-1/2 z-50  max-w-xs w-auto">
           <Comments blogId={params.slug[0]}/>
-          </div>
         </div>
       </div>
   )
