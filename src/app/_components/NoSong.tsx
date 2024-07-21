9132d5fe7b2ee2cd3915201eb89a3d2d3daf401d
Frederick Emerson <frederick82004@gmail.com>
2024-07-21 17:45:25 +0800

    more ui changes

--- src/app/_components/NoSong.tsx
+++ src/app/_components/NoSong.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function NoSong() {
   return (
     <div className="opacity-0 md:opacity-100 md:w-auto bg-gray-900 text-gray-50 px-4 py-2 rounded-full flex items-center cursor-pointer hover:bg-gray-800 ">
-        <span className="text-sm font-medium"> Frederick isn't listening to a song</span>
+        <span className="text-sm font-medium"> Frederick isn't vibing</span>
     </div>
   )
 }
